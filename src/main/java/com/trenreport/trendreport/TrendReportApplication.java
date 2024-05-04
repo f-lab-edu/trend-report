fc772ea3d80dafc184e9416905b5460a904aa7b6
SeokCheol <lsc991123@gmail.com>
2024-05-04 11:37:38 +0900

    feat: init

---
+++ src/main/java/com/trenreport/trendreport/TrendReportApplication.java
@@ -0,0 +1,13 @@
+package com.trenreport.trendreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrendReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trenreport/trendreport/TrendReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trenreport.trendreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
